19570dfe33db06f140d1076d1fff57f27912bfc4
DuhBenhur <eduardo.benhur@gmail.com>
2022-11-26 18:02:33 -0300

    inicio

---
+++ Cap02/cap02.R
@@ -0,0 +1,74 @@
+print("Estou iniciando minha caminhada na carreira de Cientista de dados")
+
+plot(1:25)
+install.packages("caret")
+install.packages("randomForest")
+install.packages("sos")
+library(sos)
+library(tidyverse)
+
+example('matplot')
+example('ggplot')
+
+
+#Operadores básicos, relacionais e lógicos
+
+#Problemas com acentuação
+
+#https://support.rstudio.com/hc/en-us/articles/200532197-chatacter-Encoding
+
+#soma 
+7+7
+
+#Subtração
+7 - 4
+
+#Multiplicação
+5 * 5
+
+#Divisao
+6 / 6
+
+#Potência
+3 ^ 2
+3 ** 2
+
+#Módulo
+
+16 %% 3
+#Operadores Relacionais
+
+#Atribuindo variáveis
+
+x = 7
+
+y = 5
+
+#Operadores
+
+x > 8
+x < 8
+x <= 8
+x >= 8
+x == 8
+x != 8
+
+#Operadores lógicos
+
+#And
+
+(x==8)&(x==6)
+(x==7)&(x==5)
+(x==8)&(x==5)
+
+#Or
+(x==8)&(x>5)
+(x==8)&(x>=6)
+
+
+#Not
+
+x > 8
+print(!x > 8)
+
+
